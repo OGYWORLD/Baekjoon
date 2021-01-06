cf901e3ed71b46e77096f090bdbac799a9dfb5ef
OGYWORLD <ogyogy1004@gmail.com>
2021-01-06 17:29:44 +0900

    2021OGY

--- 백준_2447.java
+++ 백준_2447.java
@@ -5,7 +5,7 @@ import java.io.InputStreamReader;
 import java.io.OutputStreamWriter;
 
 
-public class B24472 {
+public class B2447 {
 
 	public static void main(String[] args) throws IOException {
 		BufferedReader bf = new BufferedReader(new InputStreamReader(System.in));
